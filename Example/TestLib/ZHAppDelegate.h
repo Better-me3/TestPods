d280e0bb1e8acb44fab22830fd2dc136ea5766fd
zhang <zhang@aia.com>
2017-12-18 23:02:14 +0800

    Initial commit

---
+++ Example/TestLib/ZHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZHAppDelegate.h
+//  TestLib
+//
+//  Created by Better-me3 on 12/18/2017.
+//  Copyright (c) 2017 Better-me3. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/TestLib/ZHViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZHViewController.h
+//  TestLib
+//
+//  Created by Better-me3 on 12/18/2017.
+//  Copyright (c) 2017 Better-me3. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZHViewController : UIViewController
+
+@end
